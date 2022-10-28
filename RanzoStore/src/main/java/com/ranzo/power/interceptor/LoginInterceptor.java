52a3e406645e2b9f52d557f584fe3dda11543bb3
HyunaKim-dev <101328301+HyunaKim-dev@users.noreply.github.com>
2022-10-28 17:24:07 +0900

    com

--- RanzoStore/src/main/java/com/ranzo/power/interceptor/LoginInterceptor.java
+++ RanzoStore/src/main/java/com/ranzo/power/interceptor/LoginInterceptor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+<<<<<<< Updated upstream
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,10 +30,38 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		}
 	}
 
+=======
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+	
+	//메인 액션이 실행되기 전
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		//세션 객체 생성
+		HttpSession session=request.getSession();
+		//세션이 없으면(로그인되지 않은 상태)
+		if(session.getAttribute("userid") == null) {
+			response.sendRedirect(request.getContextPath()
+					+"/member/login.do?message=nologin");
+			return false; //메인 액션으로 가지 않음
+		}else {//로그인 했으면
+			return true; //메인 액션으로 이동
+		}
+	}
+	
+	//메인 액션이 실행된 후
+>>>>>>> Stashed changes
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		super.postHandle(request, response, handler, modelAndView);
 	}
 
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
